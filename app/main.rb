6a3c2d87c9b67f4d1b8762520ef4ad910cce18c8
Austen Jennings <austenhjennings@gmail.com>
2018-12-24 13:22:12 -0500

    added file output

--- app/main.rb
+++ app/main.rb
@@ -5,6 +5,7 @@ require_relative 'morse_code_converter'
 class Main
   @converter = MorseCodeConverter.new
   @converted = ''
+  @error = false
 
   options = {}
   OptionParser.new do |parser|
@@ -14,6 +15,9 @@ class Main
     parser.on("-m", "--morse morse", "Morse code to decrypt") do |v|
       options[:morse] = v
     end
+    parser.on("-f=s", "--output", "File to output to.") do |v|
+      options[:output] = v
+    end
 
     parser.on("-h", "--help", "Show this help message") do
       puts parser
@@ -22,11 +26,24 @@ class Main
 
   if options[:words] and options[:morse]
     @converted = "only one of morse and words can be used"
+    @error = true
   elsif options[:words]
     @converted = @converter.to_morse(options[:words])
   elsif options[:morse]
     @converted = @converter.to_characters(options[:morse])
   end
 
-  puts @converted
+  if options[:output] && !@error
+    file = options[:output]
+    File.open(file, "a+" ) do |f|
+      if options[:words]
+        f.write(options[:words])
+      else
+        f.write(options[:morse])
+      end
+      f.puts(" :: #{@converted}")
+    end
+  else
+    puts @converted
+  end
 end
